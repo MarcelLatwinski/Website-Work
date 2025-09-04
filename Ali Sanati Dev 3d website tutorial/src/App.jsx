9a866bd5ec2396cf4e1f83e05279c87c9334a0fc
Marcel Latwinski <marcellatwinski@gmail.com>
2025-09-04 16:12:39 +0100

    Added Navbar in most recent tutorial. Understood a lot of properties ive been applying and started work on the main screen

--- Ali Sanati Dev 3d website tutorial/src/App.jsx
+++ Ali Sanati Dev 3d website tutorial/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "./sections/navbar"
+import Hero from "./sections/Hero"
 
 const App = () => { //Defines a function component - reusable building block of UI - written as an arrow function - modern shorthand for writing functions in javascript
   return (
   <div className="container mx-auto max-w-7xl">
     <Navbar />
-    {/* hero */}
+    <Hero />
     {/* about */}
     {/* projects */}
     {/* experience */}
