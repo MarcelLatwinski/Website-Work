38d03861509603e85643641b1508278128a07bda
Marcel Latwinski <marcellatwinski@gmail.com>
2025-09-08 17:41:39 +0100

    Finished About section utilising already made 3d assets from Magic UI to finish off creating the tiled information section. Also used useState and clipboard.writeText to create a copy email button which is p cool

--- Ali Sanati Dev 3d website tutorial/src/sections/About.jsx
+++ Ali Sanati Dev 3d website tutorial/src/sections/About.jsx
@@ -1,8 +1,10 @@
 import { useRef } from "react";
 import Card from "../components/Card";
+import { Globe } from "../components/globe";
+import CopyEmailButton from "../components/CopyEmailButton";
+import { Frameworks } from "../components/Frameworks";
 
 const About = () => {
-
   const grid2Container = useRef();
 
   return (
@@ -29,35 +31,88 @@ const About = () => {
         </div>
         {/* Grid 2 */}
         <div className="grid-default-color grid-2">
-          <div ref = {grid2Container} className="flex items-center justify-center w-full h-full">
+          <div
+            ref={grid2Container}
+            className="flex items-center justify-center w-full h-full"
+          >
             <p className="flex items-end text-5xl text-gray-500">
               {" "}
               CODE IS CRAFT{" "}
             </p>
-            <Card containerRef={grid2Container} style={{rotate:"75deg", top: "30%", left: "20%" }} text="GRASP" />
-            <Card containerRef={grid2Container} style={{rotate:"-30deg", top: "60%", left: "45%" }} text="SOLID" />
-            <Card containerRef={grid2Container} style={{rotate:"90deg", bottom: "30%", left: "70%" }} text="Design Patterns" />
-            <Card containerRef={grid2Container} style={{rotate:"-45deg", top: "55%", left: "0%" }} text="Design Principles" />
-            <Card containerRef={grid2Container} style={{rotate:"20deg", top: "10%", left: "38%" }} text="SRP" />
-            <Card containerRef={grid2Container} style={{rotate:"30deg", top: "70%", left: "70%" }} image="assets/logos/csharp-pink.png" />
-            <Card containerRef={grid2Container} style={{rotate:"-45deg", top: "70%", left: "25%" }} image="assets/logos/dotnet-pink.png" />
-            <Card containerRef={grid2Container} style={{rotate:"-45deg", top: "5%", left: "10%" }} image="assets/logos/blazor-pink.png" />
+            <Card
+              containerRef={grid2Container}
+              style={{ rotate: "75deg", top: "30%", left: "20%" }}
+              text="GRASP"
+            />
+            <Card
+              containerRef={grid2Container}
+              style={{ rotate: "-30deg", top: "60%", left: "45%" }}
+              text="SOLID"
+            />
+            <Card
+              containerRef={grid2Container}
+              style={{ rotate: "90deg", bottom: "30%", left: "70%" }}
+              text="Design Patterns"
+            />
+            <Card
+              containerRef={grid2Container}
+              style={{ rotate: "-45deg", top: "55%", left: "0%" }}
+              text="Design Principles"
+            />
+            <Card
+              containerRef={grid2Container}
+              style={{ rotate: "20deg", top: "10%", left: "38%" }}
+              text="SRP"
+            />
+            <Card
+              containerRef={grid2Container}
+              style={{ rotate: "30deg", top: "70%", left: "70%" }}
+              image="assets/logos/csharp-pink.png"
+            />
+            <Card
+              containerRef={grid2Container}
+              style={{ rotate: "-45deg", top: "70%", left: "25%" }}
+              image="assets/logos/dotnet-pink.png"
+            />
+            <Card
+              containerRef={grid2Container}
+              style={{ rotate: "-45deg", top: "5%", left: "10%" }}
+              image="assets/logos/blazor-pink.png"
+            />
           </div>
         </div>
         {/* Grid 3 */}
         <div className="grid-black-color grid-3">
-            <div className="z-10 w-[50%]">
-                <p className="headtext">Time Zone</p>
-                <p className="subtext">I'm based in Durham / London / Polska, and open to remote work across the galaxy</p>
-            </div>
-            <figure className="absolute left-[30%] top-[10%]">
-
-            </figure>
+          <div className="z-10 w-[50%]">
+            <p className="headtext">Time Zone</p>
+            <p className="subtext">
+              I'm based in Durham / London / Polska, and open to remote work
+              across the galaxy
+            </p>
+          </div>
+          <figure className="absolute left-[30%] top-[10%]">
+            <Globe />
+          </figure>
         </div>
         {/* Grid 4 */}
-        <div className="grid-special-color grid-4"></div>
+        <div className="grid-special-color grid-4">
+          <div className="flex flex-col items-center justify-center gap-4 size-full">
+            <p className="text-center headtext">
+              Do you Want to start a Project Together?
+            </p>
+            <CopyEmailButton />
+          </div>
+        </div>
         {/* Grid 5 */}
-        <div className="grid-default-color grid-5"></div>
+        <div className="grid-default-color grid-5">
+          <div className="z-10 w-[50%]">
+            <p className="headText">I'm a Genius</p>
+            <p className="subtext">I do not specialise, I adapt. <br /> I do not know, I learn. <br /> I do not eat, I starve. <br /> Hire me, I need money.</p>
+          </div>
+          <div className="absolute inset-y-0 md:inset-y-9 w-full h-full start-[50%] md:scale-125">
+          <Frameworks />
+          </div>
+        </div>
       </div>
     </section>
   );
