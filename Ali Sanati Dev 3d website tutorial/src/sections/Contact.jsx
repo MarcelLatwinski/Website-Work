ee19d2db35425a40b6c537f6d347aa63ba56ce84
Marcel Latwinski <marcellatwinski@gmail.com>
2025-09-20 17:57:36 +0100

    Started work on the contact button. Also started making my own website using the vite-next app however realised i should probably finish this tutorial first

--- Ali Sanati Dev 3d website tutorial/src/sections/Contact.jsx
+++ Ali Sanati Dev 3d website tutorial/src/sections/Contact.jsx
@@ -1,8 +1,93 @@
+import { useState } from "react";
+
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    //Will be updated by handleChange
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    //event handler for form inputs (e is the event object)
+    setFormData({ ...formData, [e.target.name]: e.target.value }); //Dynamically use the inputs name attribute as the key in the object
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
-    <div className="Contact">
-      <h2>Contact</h2>
-    </div>
+    <section className="relative flex items-center c-space section-spacing">
+      <div className="flex flex-col items-center justify-center max-w-md p-5 mx-auto border border-white/10 rounded-2xl bg-primary">
+        <div className="flex flex-col items-start w-full gap-5 mb-10">
+          <h2 className="text-heading">Let's Talk</h2>
+          <p className="font-normal text-neutral-400">
+            Whether you're someone on my github looking at my websites, or
+            someone I am personally showing my website because im bored - I'm
+            here to help
+          </p>
+        </div>
+        <form className="w-full">
+          <div className="mb-5">
+            <label htmlFor="name" className="field-label">
+              Full Name
+            </label>
+            <input
+              id="name"
+              name="name"
+              type="text"
+              className="field-input field-input-focus"
+              placeholder="Alice Henman"
+              autoComplete="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label htmlFor="email" className="field-label">
+              Email
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              className="field-input field-input-focus"
+              placeholder="AliceHenman@gmail.com"
+              autoComplete="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label htmlFor="message" className="field-label">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              type="text"
+              rows="4"
+              className="field-input field-input-focus"
+              placeholder="Tell Marcel how COOL he is"
+              autoComplete="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full px-1 py-3 text-lg text-center rounded-md cursor-pointer bg-radial from-lavender to-royal hover-animation"
+          >
+            Send
+          </button>
+        </form>
+      </div>
+    </section>
   );
 };
 
