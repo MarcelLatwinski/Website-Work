f64694ef2dc47e8e5aaba7a94b3dbfa1dc32e9a1
Marcel Latwinski <marcellatwinski@gmail.com>
2025-09-21 14:11:49 +0100

    Finished the contact section using emailjs to contact my work email, with a customised alert mechanic as well as a particles background from magic ui.

--- Ali Sanati Dev 3d website tutorial/src/sections/Contact.jsx
+++ Ali Sanati Dev 3d website tutorial/src/sections/Contact.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import emailjs from "@emailjs/browser";
+import Alert from "../components/Alert";
+import { Particles } from "../components/Particles";
 
 const Contact = () => {
   const [formData, setFormData] = useState({
@@ -8,18 +11,66 @@ const Contact = () => {
     message: "",
   });
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertType, setAlertType] = useState("success");
+  const [alertMessage, setAlertMessage] = useState("");
+
   const handleChange = (e) => {
     //event handler for form inputs (e is the event object)
     setFormData({ ...formData, [e.target.name]: e.target.value }); //Dynamically use the inputs name attribute as the key in the object
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData);
+  const showAlertMessage = (type, message) => {
+    setAlertType(type);
+    setAlertMessage(message);
+    setShowAlert(true);
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 5000);
+  };
+
+  const handleSubmit = async (e) => {
+    //async adds a promise - will call the function in the future successfully or not
+    e.preventDefault(); //stop browser frefreshing page when submit
+    setIsLoading(true);
+
+    try {
+      console.log("Form Submitted :", formData);
+      await emailjs.send(
+        "service_c8lrnso",
+        "template_iuthnfc",
+        {
+          from_name: formData.name,
+          to_name: "Marcel",
+          from_email: formData.email,
+          to_email: "MarcelLatwinski.work@gmail.com",
+          message: formData.message,
+        },
+        import.meta.env.VITE_EMAIL_PUBLIC_KEY
+      );
+      setIsLoading(false);
+      showAlertMessage("Success", "Your message has been sent");
+      setFormData({ name: "", email: "", message: "" });
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error);
+      showAlertMessage("Failed", "Something went wrong D;");
+    }
+    //service_c8lrnso
+    //template_iuthnfc
   };
 
   return (
     <section className="relative flex items-center c-space section-spacing">
+      <Particles
+        className="absolute inset-0 -z-50"
+        quantity={100}
+        ease={80}
+        color={"#ffffff"}
+        refresh
+      />
+      {showAlert && <Alert type={alertType} text={alertMessage} />}
       <div className="flex flex-col items-center justify-center max-w-md p-5 mx-auto border border-white/10 rounded-2xl bg-primary">
         <div className="flex flex-col items-start w-full gap-5 mb-10">
           <h2 className="text-heading">Let's Talk</h2>
@@ -29,7 +80,7 @@ const Contact = () => {
             here to help
           </p>
         </div>
-        <form className="w-full">
+        <form className="w-full" onSubmit={handleSubmit}>
           <div className="mb-5">
             <label htmlFor="name" className="field-label">
               Full Name
@@ -83,7 +134,7 @@ const Contact = () => {
             type="submit"
             className="w-full px-1 py-3 text-lg text-center rounded-md cursor-pointer bg-radial from-lavender to-royal hover-animation"
           >
-            Send
+            {!isLoading ? "Send" : "Sending..."}
           </button>
         </form>
       </div>
--- Ali Sanati Dev 3d website tutorial/src/sections/Footer.jsx
+++ Ali Sanati Dev 3d website tutorial/src/sections/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="Footer">
-      <h2>Footer</h2>
+      
     </div>
   );
 };
